a89e9d89c9497f83a407b0cda612ce7fde54b9a5
Hrissy-art <hristinastankova89@gmail.com>
2023-08-31 10:13:26 +0200

    Forms

---
+++ js/mouse.js
@@ -0,0 +1,8 @@
+"use strict"
+
+const mousemove = document.querySelector(".mousemove")
+window.addEventListener("mousemove", (e) => {
+mousemove.style.left= e.pageX +"px";
+mousemove.style.top= e.pageY +"px";
+
+})
---
+++ js/utils.js
@@ -0,0 +1,7 @@
+"use strict";
+
+function isNumeric(str)
+{
+    /* Source : https://codingbeautydev.com/blog/javascript-check-if-string-contains-only-numbers/ */
+    return /^\d+$/.test(str);
+}
